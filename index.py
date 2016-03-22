58f57cde64693d385ee1d897fff34ea51d570b57
Eamon Banta <banta.eamon@gmail.com>
2016-03-22 15:03:22 -0400

    remove .pyc files from git

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from datetime import date
+fom datetime import date
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def main(month='1', year='2012'):
     # builds calendar using Python dates
     calendar = Calendar(month, year)
     cal_html = calendar.build()
-    
+
     # provide values for use in building the Javascript calendar
     month = int(month)
     months = (
@@ -22,7 +22,7 @@ def main(month='1', year='2012'):
     month_name = months[month - 1]
     prev_year = int(year)
     next_year = int(year)
-    
+
     # used for switching between months (links)
     prev_month = month - 1
     next_month = month + 1
@@ -32,19 +32,19 @@ def main(month='1', year='2012'):
     elif month == 12:
         next_month = 1
         next_year += 1
-        
+
     prev_month_name = months[prev_month - 1]
     next_month_name = months[next_month - 1]
-    
+
     # Javascript calculates dates differently, so subtract 1
     month -= 1
-    
-    return render_template('calendar', 
-                           year=year, month=month, 
+
+    return render_template('calendar',
+                           year=year, month=month,
                            month_name=month_name,
                            prev_month=prev_month,
                            prev_month_name=prev_month_name,
-                           next_month=next_month, next_year=next_year, 
+                           next_month=next_month, next_year=next_year,
                            next_month_name=next_month_name,
                            cal_html=cal_html)
 
